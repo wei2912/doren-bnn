16e303054f6019749bd7f4f255ae92cf954f0b85
Ng Wei En <weien1292@gmail.com>
2022-06-10 15:54:27 +0800

    main.rs: Abstract out encoder printing

--- doren-bnn-concrete/src/main.rs
+++ doren-bnn-concrete/src/main.rs
@@ -11,6 +11,19 @@ const BSK_LEVEL: usize = 3;
 const KSK_BASE_LOG: usize = 2;
 const KSK_LEVEL: usize = 7;
 
+fn print_encoder(encoder: &Encoder) {
+    println!("{:?}", encoder);
+    println!(
+        "{:?}",
+        (
+            encoder.get_granularity(),
+            encoder.get_min(),
+            encoder.get_max(),
+            encoder.get_size()
+        )
+    );
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     let encoder = Encoder::new(-1.0, 1.0, 1, 7)?;
     let messages = vec![-1.0, 1.0, 1.0, 1.0, -1.0, -1.0, 1.0, 1.0, 1.0];
@@ -57,11 +70,13 @@ fn main() -> Result<(), Box<dyn Error>> {
         println!("Secret keys generated.");
         (sk_out, ksk, bsk)
     };
+    println!();
 
     let mut c1 = VectorLWE::encode_encrypt(&sk_out, &messages, &encoder)?;
     let o1 = c1.decrypt_decode(&sk_out)?;
     println!("After encryption + decryption: {:?}", o1);
-    println!("{:?}", c1.encoders[0]);
+    print_encoder(&c1.encoders[0]);
+    println!();
 
     let weights = vec![1.0, -1.0, -1.0, 1.0, 1.0, 1.0, -1.0, -1.0, 1.0];
     for i in 0..weights.len() - 1 {
@@ -73,10 +88,13 @@ fn main() -> Result<(), Box<dyn Error>> {
     let o1 = c1.decrypt_decode(&sk_out)?;
     println!("Weights: {:?}", weights);
     println!("After weighting: {:?}", o1);
+    println!();
 
     let c1_sum = c1.sum_with_padding()?;
     let o1_sum = c1_sum.decrypt_decode(&sk_out)?;
     println!("Sum: {:?}", o1_sum);
+    print_encoder(&c1_sum.encoders[0]);
+    println!();
 
     const THRESHOLD: f64 = -6.0; // arbitrary threshold as "learnable parameter"
     let c2 = c1_sum.keyswitch(&ksk)?.bootstrap_nth_with_function(
@@ -88,18 +106,8 @@ fn main() -> Result<(), Box<dyn Error>> {
     let o2 = c2.decrypt_decode(&sk_out)?;
     println!("Threshold: {:?}", THRESHOLD);
     println!("ReLU + bootstrap: {:?}", o2);
-
-    let encoder = &c2.encoders[0];
-    println!("{:?}", encoder);
-    println!(
-        "{:?}",
-        (
-            encoder.get_granularity(),
-            encoder.get_min(),
-            encoder.get_max(),
-            encoder.get_size()
-        )
-    );
+    print_encoder(&c2.encoders[0]);
+    println!();
 
     Ok(())
 }
